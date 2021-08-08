1e93ebb7479de1e1ed6c6b545f569c4e05806da6
jrmoynihan <jrmoynihan@gmail.com>
2021-08-08 19:17:02 -0400

    matchup status and scores added; clock tweaks

---
+++ src/lib/components/containers/micro/GameTime.svelte
@@ -0,0 +1,15 @@
+<script lang='ts'>
+import type { Timestamp } from "@firebase/firestore";
+
+export let timestamp: Timestamp
+
+</script>
+
+<!-- {Intl.DateTimeFormat('en-US',{weekday:'long'}).format(timestamp.toDate().getDay())} -->
+<p>{timestamp.toDate().toDateString()}</p>
+<p>@ {timestamp.toDate().toLocaleTimeString([], { timeStyle: 'short' })}</p>
+
+
+<style lang='scss'>
+
+</style>
