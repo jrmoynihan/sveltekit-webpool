07310be6d3ded4bff8d432cd8b2821ef4acde98a
jrmoynihan <jrmoynihan@gmail.com>
2022-04-30 12:53:55 -0400

    removed old accordion and portal

--- src/routes/admin/sandbox.svelte
+++ src/routes/admin/sandbox.svelte
@@ -1,5 +1,5 @@
 <script script lang="ts">
-	import AccordionDetailsv2 from '$lib/components/containers/accordions/AccordionDetails.svelte';
+	import AccordionDetails3 from '$lib/components/containers/accordions/AccordionDetails3.svelte';
 	import MultiToggleSwitch from '$lib/components/switches/MultiToggleSwitch.svelte';
 	import DriveChart from '$lib/images/DriveChart.svelte';
 	import type { CompetitorESPN, ESPNGame, ESPNTeamData, Game } from '$scripts/classes/game';
@@ -17,14 +17,7 @@
 	import { getDocs, query, where } from 'firebase/firestore';
 
 	let open = false;
-	let isExpanding = false;
-	let isCollapsing = false;
 
-	let myItems = [
-		{ label: 'Alpha', value: 1 },
-		{ label: 'Beta', value: 'second' },
-		{ label: 'Delta', value: { stuff: 'thingy' } }
-	];
 	let otherItems = [
 		{ label: 'One', value: 1 },
 		{ label: 'Two', value: 2 },
@@ -66,21 +59,10 @@
 		const awayColors: { color: string; altColor: string } = await getTeamColors(awayCompetitor);
 		return { game, awayTeam, homeTeam, homeColors, awayColors, drivesRef, gameData: gameData.data };
 	};
-	let portal: HTMLElement;
-	function activatePortal() {
-		portal.activate();
-	}
-	if(browser && portal){
-		window.addEventListener('portalactivate',(evt) => {
-			const predecessor = evt.adoptPredecessor();
-		}
-		)
-	}
 </script>
 
 <MultiToggleSwitch items={otherItems} selectedItem={otherItems[1]} titleText="This is the Title" />
 
-<!-- <MultiToggleSwitch items={viewPreferences} adminOnly={true} titleText="Admin Options" /> -->
 <MultiToggleSwitch
 	items={viewPreferences}
 	titleText="all true"
@@ -94,48 +76,11 @@
 	<DriveChart {awayTeam} {homeTeam} {homeColors} {awayColors} {game} {drivesRef} {gameData} />
 {/await}
 
-<AccordionDetailsv2 bind:open bind:isExpanding bind:isCollapsing>
+<AccordionDetails3 bind:isOpen={open}>
 	<div slot="summary">
-		<h2>Open: {open} isExpanding: {isExpanding} isCollapsing: {isCollapsing}</h2>
+		<h2>Open: {open}</h2>
 	</div>
 	<div slot="content">
 		<p>Content</p>
 	</div>
-</AccordionDetailsv2>
-
-<portal
-	bind:this={portal}
-	id="example"
-	title="test"
-	src="https://www.youtube.com/embed/95WCap877oM"
-	style="width: 100%; height: 100%;"
-	on:click={activatePortal}
-/>
-
-<style lang="scss">
-	portal {
-		position: fixed;
-		width: 80%;
-		height: 80%;
-		opacity: 0;
-		box-shadow: 0 0 20px 10px #999;
-		transform: scale(0.4);
-		transform-origin: bottom left;
-		bottom: 20px;
-		left: 20px;
-		animation-name: fade-in;
-		animation-duration: 1s;
-		animation-delay: 2s;
-		animation-fill-mode: forwards;
-		transition: transform 0.4s;
-		// transform: scale(1.0) translateX(-20px) translateY(20px);
-	}
-	@keyframes fade-in {
-		0% {
-			opacity: 0;
-		}
-		100% {
-			opacity: 1;
-		}
-	}
-</style>
+</AccordionDetails3>
