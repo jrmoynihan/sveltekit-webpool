74dffa9b6542342ef780d617162e2a932417df74
jrmoynihan <jrmoynihan@gmail.com>
2022-04-30 23:35:55 -0400

    fixed server cookie hook and locals interface

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,4 +1,8 @@
-/// <reference types="@sveltejs/kit" />
+// <reference types="@sveltejs/kit" />
+// See https://kit.svelte.dev/docs/types#app
+// for information about these interfaces
+
+// Add the missing methods that are available on the <dialog> HTML interface (see https://developer.mozilla.org/en-US/docs/Web/API/HTMLDialogElement)
 interface HTMLDialogElement extends HTMLDialogElement {
 	close(returnValue?: string): void;
 	show(): void;
@@ -10,4 +14,15 @@ declare let HTMLDialogElement: {
 };
 interface Window extends Window {
 	HTMLDialogElement: HTMLDialogElement;
+}
+declare namespace App {
+	interface Locals {
+		userid: string;
+	}
+
+	// interface Platform {}
+
+	// interface Session {}
+
+	// interface Stuff {}
 }
