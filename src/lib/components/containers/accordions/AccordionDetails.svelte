ac6ce746692b9668a0c22a28a7100aecc449398e
jrmoynihan <jrmoynihan@gmail.com>
2022-05-08 19:55:12 -0400

    feat(Components): :technologist: Dramatically simplified the Accordion component API and usage and styling.
    The old accordion made use of a JS class that added web animations to a <details> element in a complex manner. The new "accordion" simply utiilizes a div with a button and applies CSS styles/transitions.

--- src/lib/components/containers/accordions/AccordionDetails.svelte
+++ src/lib/components/containers/accordions/AccordionDetails.svelte
@@ -1,146 +1,22 @@
 <script lang="ts">
 	import type { IconDefinition } from '@fortawesome/fontawesome-common-types';
-	import { createEventDispatcher } from 'svelte';
 	import Fa from 'svelte-fa';
+	import { cubicInOut } from 'svelte/easing';
+	import { slide } from 'svelte/transition';
 
 	export let expandTitle: string = '';
 	export let showArrow: boolean = false;
 	export let customExpandIcon: IconDefinition = null;
 	export let iconClass: string = 'fa-CaretDown';
-	export let expandDuration: number = 300;
 	export let customContentStyles: string = '';
-	export let customDetailsStyles: string = '';
 	export let customSummaryStyles: string = '';
 	export let cloudyBackground = false;
 	export let frostedGlass = true;
 	export let open = false;
 	export let adminOnly = false;
-	export let overflowY = 'auto';
-	export let overflow_X = 'auto';
-	let detailsElement: HTMLDetailsElement;
-	let summaryElement: HTMLElement;
-	let contentElement: HTMLElement;
-	let animation: Animation = null;
-	export let isCollapsing: boolean = false;
-	export let isExpanding: boolean = false;
+	export let slideParameters = { duration: 500, easing: cubicInOut };
 
-	// $: if (detailsElement) {
-	// 	summaryElement = detailsElement.querySelector('summary');
-	// }
-	// $: if (detailsElement) {
-	// 	contentElement = detailsElement.querySelector('.content');
-	// }
-
-	const dispatch = createEventDispatcher();
-
-	const clicked = () => {
-		dispatch('click');
-		toggle();
-	};
-
-	export function toggle() {
-		if (!open && (isCollapsing || !isExpanding)) {
-			// alert('expanding');
-			expand();
-		} else if (isExpanding || open) {
-			collapse();
-		}
-	}
-	export function expand() {
-		isExpanding = true;
-		isCollapsing = false;
-		// Add an overflow on the <details> to avoid content overflowing
-		detailsElement.style.overflow = 'hidden';
-		// Apply a fixed height on the element
-		detailsElement.style.height = `${detailsElement.offsetHeight}px`;
-		// Force the [open] attribute on the details element
-		detailsElement.open = true;
-		// Wait for the next frame to call the expand function
-		window.requestAnimationFrame(() => startExpand());
-	}
-	function startExpand() {
-		// Set the element as "being expanding"
-		isExpanding = true;
-		// Get the current fixed height of the element
-		const startHeight = `${detailsElement.offsetHeight}px`;
-		// Calculate the open height of the element (summary height + content height)
-		const endHeight = `${summaryElement.offsetHeight + contentElement.offsetHeight}px`;
-
-		// If there is already an animation running
-		if (animation) {
-			// Cancel the current animation
-			animation.cancel();
-		}
-
-		// Start a WAAPI animation
-		animation = detailsElement.animate(
-			{
-				// Set the keyframes from the startHeight to endHeight
-				height: [startHeight, endHeight]
-			},
-			{
-				duration: expandDuration,
-				easing: 'ease-out'
-			}
-		);
-		// When the animation is complete, call onAnimationFinish()
-		animation.onfinish = () => onAnimationFinish(true);
-		// If the animation is cancelled, isExpanding variable is set to false
-		animation.oncancel = () => (isExpanding = false);
-	}
-
-	export function collapse() {
-		isCollapsing = true;
-		isExpanding = false;
-
-		// Add an overflow on the <details> to avoid content overflowing
-		detailsElement.style.overflow = 'hidden';
-
-		// Store the current height of the element
-		const startHeight = `${detailsElement.offsetHeight}px`;
-
-		// Calculate the height of the summary
-		const endHeight = `${summaryElement.offsetHeight}px`;
-
-		// If there is already an animation running
-		if (animation) {
-			// Cancel the current animation
-			animation.cancel();
-		}
-
-		// Start a WAAPI animation
-		animation = detailsElement.animate(
-			{
-				// Set the keyframes from the startHeight to endHeight
-				height: [startHeight, endHeight]
-			},
-			{
-				duration: expandDuration,
-				easing: 'ease-out'
-			}
-		);
-
-		// When the animation is complete, call onAnimationFinish()
-		animation.onfinish = () => {
-			onAnimationFinish(false);
-		};
-		// If the animation is cancelled, isCollapsing variable is set to false
-		animation.oncancel = () => (isCollapsing = false);
-	}
-
-	function onAnimationFinish(isNowOpen: boolean) {
-		// Set the open attribute based on the parameter
-		open = isNowOpen;
-		// Clear the stored animation
-		animation = null;
-		// Reset isCollapsing & isExpanding
-		isCollapsing = false;
-		isExpanding = false;
-		// Remove the overflow hidden and the fixed height
-		detailsElement.style.height = '';
-		detailsElement.style.overflowY = overflowY;
-		detailsElement.style.overflowX = overflow_X;
-	}
+	export const toggle = () => (open = !open);
 
 	const checkforSpace = async (e: KeyboardEvent & { currentTarget: EventTarget & HTMLElement }) => {
 		if (e.code === 'Space') {
@@ -149,120 +25,88 @@
 	};
 </script>
 
-<details
-	bind:this={detailsElement}
-	class:cloudyBackground
-	class:frostedGlass
-	class:adminOnly
-	bind:open
-	on:click|self|preventDefault|stopPropagation={clicked}
-	on:keydown|preventDefault={(e) => checkforSpace(e)}
-	tabindex="0"
-	style={customDetailsStyles}
->
-	<summary
-		bind:this={summaryElement}
-		on:click|self|preventDefault|stopPropagation={clicked}
-		on:keydown|preventDefault={(e) => checkforSpace(e)}
+<div class="accordion-container">
+	<button
+		class:cloudyBackground
+		class:frostedGlass
 		class:adminOnly
-		class={showArrow ? '' : 'hideArrow'}
-		style="display:{showArrow ? 'list-item' : 'flex'}; border-radius:{open
-			? '0'
-			: 'inherit'}; {customSummaryStyles}"
+		on:click={toggle}
+		aria-expanded={open}
+		on:keydown|preventDefault={(e) => checkforSpace(e)}
+		tabindex="0"
+		style={customSummaryStyles}
 	>
-		{#if customExpandIcon}
-			<Fa icon={customExpandIcon} class={iconClass} />
-		{/if}
 		{#if expandTitle !== ''}
 			{expandTitle}
 		{/if}
 		<slot name="summary" />
-	</summary>
-
-	<div bind:this={contentElement} class="content" style={customContentStyles}>
-		<slot name="content" />
-	</div>
-</details>
+		{#if customExpandIcon}
+			<Fa icon={customExpandIcon} class={iconClass} rotate={open ? 90 : 0} />
+		{:else if showArrow}
+			<svg
+				style="tran"
+				width="20"
+				height="20"
+				fill="none"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				viewBox="0 0 24 24"
+				stroke="currentColor"><path d="M9 5l7 7-7 7" /></svg
+			>
+		{/if}
+	</button>
+	{#if open}
+		<div class="content" style={customContentStyles} transition:slide={slideParameters}>
+			<slot name="content" />
+		</div>
+	{/if}
+</div>
 
 <style lang="scss">
-	// @import 'src/Styles/Mixins.scss';
-	details,
-	summary {
-		@include defaultTransition;
-		@include rounded;
-		outline: none;
-		width: 100%;
-	}
-	details {
-		@include accelerate;
-		@include thinnestBorder;
-		@include noOutline;
-		color: var(--text);
-		&:hover,
-		&:focus-within {
-			@include dayShadow;
-		}
-		&.cloudyBackground {
-			@include cloudyBackground;
-		}
-		&.frostedGlass {
-			@include frostedGlass;
-		}
+	button {
 		@include normalShadow;
-
-		&[open] {
-			& summary ~ * {
-				@include accelerate;
-				animation: sweep 0.5s ease-in-out;
-			}
-		}
-	}
-	@keyframes sweep {
-		0% {
-			opacity: 0;
-			transform: translateY(-20px);
-		}
-		100% {
-			opacity: 1;
-			transform: translateY(0);
-		}
-	}
-
-	summary {
-		color: black;
-		padding: 0.5em;
-		transition: border-radius 0ms;
+		box-sizing: border-box;
+		border: none;
+		background: none;
+		display: grid;
+		color: inherit;
+		font: inherit;
 		cursor: pointer;
-		justify-content: center;
-		position: sticky;
-		top: 0;
-		background: white;
-		z-index: 1;
-		&::marker,
-		&::-webkit-details-marker {
-			&::before {
-				box-sizing: border-box;
-				width: 0;
-				height: 0;
-				border-top: 60px solid transparent;
-				border-bottom: 60px solid transparent;
-				border-left: 60px solid black;
+		margin: 0;
+		padding: 0.5em;
+		gap: 0.5rem;
+		justify-items: center;
+		align-items: center;
+		grid-template-columns: 1fr auto;
+		transition: border-radius 800ms ease-in-out, background-color 300ms ease-in-out;
+		border-radius: 1rem;
+		width: 100%;
+		&[aria-expanded='true'] {
+			border-radius: 1rem 1rem 0 0;
+			transition: border-radius 0s ease-in-out, background-color 300ms ease-in-out;
+			&:hover,
+			&:focus-within {
+				@include dayShadow;
 			}
-		}
-	}
-
-	.hideArrow {
-		&::marker,
-		&::-webkit-details-marker {
-			content: '';
+			&.cloudyBackground {
+				@include cloudyBackground;
+			}
+			&.frostedGlass {
+				@include frostedGlass;
+			}
 		}
 	}
 	.content {
-		box-sizing: border-box;
-		padding: 1rem;
-		width: 100%;
+		border-radius: 0 0 1rem 1rem;
+		transition: background-color 300ms ease-in-out;
 	}
-	.adminOnly {
-		@include admin;
+
+	svg {
+		transition: transform 0.2s ease-in;
+	}
+
+	[aria-expanded='true'] svg {
+		transform: rotate(0.25turn);
 	}
 </style>
