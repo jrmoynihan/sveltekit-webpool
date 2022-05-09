28c90b1ce2b7196cda9c7a9c6770d12b29e459a4
jrmoynihan <jrmoynihan@gmail.com>
2022-05-09 02:01:56 -0400

    removed old props; added player_pool filter option

--- src/lib/components/containers/admin/adminSelectors.svelte
+++ src/lib/components/containers/admin/adminSelectors.svelte
@@ -2,72 +2,48 @@
 	import PlayerSelect from '$lib/components/selects/PlayerSelect.svelte';
 	import WeekSelect from '$lib/components/selects/WeekSelect.svelte';
 	import YearSelect from '$lib/components/selects/YearSelect.svelte';
-	import type { Game } from '$scripts/classes/game';
-	import type { Player } from '$classes/player';
 	import { largerThanMobile } from '$scripts/store';
-	import { getAllGames } from '$scripts/weekly/weeklyAdmin';
-	import { getWeeklyPlayers } from '$scripts/weekly/weeklyPlayers';
-	import { faSync } from '@fortawesome/free-solid-svg-icons';
-	import Fa from 'svelte-fa';
 	import AdminExpandSection from './adminExpandSection.svelte';
+	import SeasonTypeSelect from '$lib/components/selects/SeasonTypeSelect.svelte';
+	import type { PoolsToQuery } from '$lib/scripts/types/types';
 
-	export let playersPromise: Promise<Player[]>;
-	export let gamePromise: Promise<Game[]>;
-	export let selectedYear: number;
-	export let selectedPlayer: Player;
-	let minColumns: string | number = '40%';
-	let startOpen = true;
-	let customContentStyles: string;
+	export let player_pool: PoolsToQuery = 'all';
+	export let minColumns: string | number = '40%';
+	export let startOpen = true;
+	export let customContentStyles: string = null;
+	export let customSummaryStyles: string = null;
 
 	$: customContentStyles = $largerThanMobile ? 'grid-column:span 2;' : undefined;
 </script>
 
-<AdminExpandSection summaryText="Selectors" bind:minColumns {startOpen} {customContentStyles}>
-	<label for="refresh" class="span-2">
-		<p>Refresh Players and Games</p>
-		<button
-			id="refresh"
-			class="refresh"
-			on:click={async () => {
-				playersPromise = getWeeklyPlayers();
-				gamePromise = getAllGames();
-				// maxWeekPromise = getMaxGameWeek();
-			}}
-		>
-			<Fa icon={faSync} />
-		</button>
-	</label>
+<AdminExpandSection
+	summaryText="Selectors"
+	bind:minColumns
+	{startOpen}
+	{customContentStyles}
+	{customSummaryStyles}
+>
 	<label for="week-select">
 		<p>Week</p>
 		<WeekSelect />
 	</label>
+	<label for="season-type-select">
+		<p>Season Type</p>
+		<SeasonTypeSelect />
+	</label>
 	<label for="year-select">
 		<p>Year</p>
-		<YearSelect bind:selectedYear />
+		<YearSelect />
 	</label>
-
-	{#await playersPromise}
-		Loading players...
-	{:then}
-		<label for="player-select" class="span-2">
-			<p>Player</p>
-			<PlayerSelect bind:selectedPlayer bind:playersPromise />
-		</label>
-	{/await}
+	<label for="player-select">
+		<p>Player</p>
+		<PlayerSelect {player_pool} />
+	</label>
 </AdminExpandSection>
 
 <style lang="scss">
-	button {
-		@include styledButton;
-	}
-	.refresh {
-		max-width: max-content;
-	}
 	label {
 		display: grid;
 		justify-items: center;
-	}
-	.span-2 {
-		grid-column: span 2;
 	}
 </style>
