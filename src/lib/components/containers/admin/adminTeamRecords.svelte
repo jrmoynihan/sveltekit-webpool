79402ac46735374ae65dc07a835f2de768662e7e
jrmoynihan <jrmoynihan@gmail.com>
2022-05-09 02:10:55 -0400

    moved resetTeamRecords back to local component

--- src/lib/components/containers/admin/adminTeamRecords.svelte
+++ src/lib/components/containers/admin/adminTeamRecords.svelte
@@ -1,26 +1,43 @@
 <script lang="ts">
 	import DeletionButton from '$lib/components/buttons/DeletionButton.svelte';
-
 	import StyledButton from '$lib/components/buttons/StyledButton.svelte';
-
+	import { defaultToast } from '$lib/scripts/toasts';
 	import { updateTeamRecordsFromESPN } from '$scripts/scorePicks';
-	import { largerThanMobile } from '$scripts/store';
-	import { resetTeamRecords } from '$scripts/teams';
+	import { all_teams, largerThanMobile, selected_year } from '$scripts/store';
+	import { updateDoc } from '@firebase/firestore';
 	import AdminExpandSection from './adminExpandSection.svelte';
 
-	export let selectedYear: number = new Date().getFullYear();
+	export let customContentStyles = null;
+	export let customSummaryStyles = null;
+
+	const resetTeamRecords = async (skipConfirmation = false) => {
+		let proceed: boolean;
+		skipConfirmation ? (proceed = true) : (proceed = false);
+		proceed = confirm('Are you sure you want to reset the team records?');
+		if (proceed) {
+			$all_teams.forEach((team) => {
+				updateDoc(team.docRef, { wins: 0, losses: 0, ties: 0 });
+			});
+			defaultToast({
+				title: 'Records Reset!',
+				msg: 'All team records have been reset to 0-0-0 (Wins-Losses-Ties).'
+			});
+		}
+	};
 
 	let minColumns: string | number;
 	$: minColumns = $largerThanMobile ? 0 : '40%';
 </script>
 
-<AdminExpandSection summaryText="NFL Teams" bind:minColumns>
+<AdminExpandSection
+	summaryText="NFL Teams"
+	bind:minColumns
+	{customContentStyles}
+	{customSummaryStyles}
+>
 	<StyledButton
 		on:click={() => {
-			// const proceed = confirm('Have you already updated the game winners?  If so, click OK.');
-			// if (proceed) {
-			updateTeamRecordsFromESPN(selectedYear);
-			// }
+			updateTeamRecordsFromESPN($selected_year);
 		}}>Update Team Records</StyledButton
 	>
 	<DeletionButton on:click={() => resetTeamRecords()}>Reset Team Records</DeletionButton>
