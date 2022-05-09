c2d85ce92b36471aaa9e635b93cf6e3508615ae3
jrmoynihan <jrmoynihan@gmail.com>
2022-05-08 20:05:56 -0400

    style(Naming): :truck: Renamed selectedWeek and selectedYear stores to use snake-case.
    Snake-case naming seems to provide better readability throughout the project.

--- src/lib/components/containers/admin/adminGamesFunctions.svelte
+++ src/lib/components/containers/admin/adminGamesFunctions.svelte
@@ -2,20 +2,19 @@
 	import StyledButton from '$lib/components/buttons/StyledButton.svelte';
 	import DeletionButton from '$lib/components/buttons/DeletionButton.svelte';
 	import { removeWinnersFromGames, updateGamesAndATSWinners } from '$scripts/scorePicks';
-	import { largerThanMobile, selectedWeek } from '$scripts/store';
+	import { largerThanMobile, selected_week, selected_year } from '$scripts/store';
 	import AdminExpandSection from './adminExpandSection.svelte';
-
-	export let selectedYear: number;
 
 	let minColumns: string | number;
 	$: minColumns = $largerThanMobile ? 0 : '40%';
 </script>
 
 <AdminExpandSection summaryText="Games" bind:minColumns>
-	<StyledButton on:click={() => updateGamesAndATSWinners($selectedWeek)}
-		><span>Find Winners of <b>Week {$selectedWeek}</b> NFL Games</span></StyledButton
+	<StyledButton on:click={() => updateGamesAndATSWinners($selected_week)}
+		><span>Find Winners of <b>Week {$selected_week}</b> NFL Games</span></StyledButton
 	>
-	<DeletionButton on:click={() => removeWinnersFromGames($selectedWeek, selectedYear)}>
-		<span>Remove Game Winners For <b>Week {$selectedWeek}, {selectedYear}</b></span></DeletionButton
+	<DeletionButton on:click={() => removeWinnersFromGames($selected_week, $selected_year)}>
+		<span>Remove Game Winners For <b>Week {$selected_week}, {$selected_year}</b></span
+		></DeletionButton
 	>
 </AdminExpandSection>
