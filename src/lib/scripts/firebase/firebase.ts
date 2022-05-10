d135ace1d8b80ec3e2255b473ea7ad1a572457a4
jrmoynihan <jrmoynihan@gmail.com>
2022-05-10 01:30:24 -0400

    try with vite meta import

--- src/lib/scripts/firebase/firebase.ts
+++ src/lib/scripts/firebase/firebase.ts
@@ -2,9 +2,9 @@ import { getApps, getApp, initializeApp, type FirebaseApp } from 'firebase/app';
 import { getAuth, type Auth } from '@firebase/auth';
 import { Firestore, getFirestore } from '@firebase/firestore';
 
-const API_KEY: string = import.meta.env.PROD ? process.env.FIREBASE_API_KEY : import.meta.env.VITE_API_KEY as string;
+const API_KEY: string = import.meta.env.PROD ? process.env.FIREBASE_API_KEY : import.meta.env.API_KEY as string;
 const firebaseConfig = {
-	apiKey: process.env.FIREBASE_API_KEY,
+	apiKey: import.meta.env.API_KEY,
 	authDomain: 'tonyswebpool.firebaseapp.com',
 	databaseURL: 'https://tonyswebpool.firebaseio.com',
 	projectId: 'tonyswebpool',
