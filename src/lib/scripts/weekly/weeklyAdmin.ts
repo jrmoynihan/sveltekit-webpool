e19de62269e9fdf5cf0fa70500b55f1e59e0fb99
jrmoynihan <jrmoynihan@gmail.com>
2022-05-09 23:10:35 -0400

    refactor(DX / Ease of Use): :technologist: Removed hacky optional params and if clauses. Typed function input options.
    If statements incurred immediate tech debt.  More flexible input types, like an array of QueryConstraints allow less rigid use and less maintenance.

--- src/lib/scripts/weekly/weeklyAdmin.ts
+++ src/lib/scripts/weekly/weeklyAdmin.ts
@@ -18,72 +18,59 @@ import { updateDoc, deleteDoc, doc, getDocs, query, setDoc, where, type QueryCon
 import { findCurrentWeekOfSchedule } from '$lib/scripts/schedule';
 import { getConsensusSpread } from '$lib/scripts/dataFetching';
 import { toast } from '@zerodevx/svelte-toast';
+import type { SeasonBoundDoc } from '../classes/seasonBound';
+import { makeNumericArrayOfDesiredLength } from '../functions';
 
-export const getAllGames = async (showToast = true) => {
+export const getAllGames = async (showToast = false): Promise<Game[]> => {
 	try {
 		const games: Game[] = [];
 		const q = query(scheduleCollection);
 		const querySnapshot = await getDocs(q.withConverter(gameConverter));
+		
 		querySnapshot.forEach((doc) => {
 			const docRef = doc.ref;
 			const id = doc.id;
 			const game = new Game({ id, docRef, ...doc.data() });
 			games.push(game);
 		});
+		
 		const title = 'Got All Games!';
 		const msg = 'Retrieved all game documents from the Schedule collection.';
-		if (showToast) {
-			defaultToast({ title, msg });
-		}
-		myLog({ msg, traceLocation: true });
+		showToast ? LogAndToast({ title, msg }) : myLog({ msg, traceLocation: true });
+		
 		return games;
 	} catch (error) {
 		const msg = `Encountered an error while trying to get all game documents.  Check the console for more info. ${error}`;
 		errorToast({msg});
-		myError({ location: 'weeklyAdmin.ts', function_name: 'getAllGames', error, msg });
+		myError({ error, msg });
 	}
 };
-export const getSpecificGames = async (
-	selectedWeek: number,
-	selectedYear: number,
-	showToast = false
-) => {
+type getSpecificGameOptions = {
+	queryConstraints: QueryConstraint[];
+	showToast?: boolean;
+}
+export const getSpecificGames = async (input : getSpecificGameOptions) => {
 	try {
+		const { queryConstraints, showToast } = input;
 		const games: Game[] = [];
-		const wheres = [where('week', '==', selectedWeek), where('year', '==', selectedYear)];
-		const q = query(scheduleCollection, ...wheres);
+		const q = query(scheduleCollection, ...queryConstraints);
 		const querySnapshot = await getDocs(q.withConverter(gameConverter));
+		
 		querySnapshot.forEach((doc) => {
 			const docRef = doc.ref;
 			const id = doc.id;
 			const game = new Game({ id, docRef, ...doc.data() });
 			games.push(game);
 		});
-		const title = `Got Games for Week ${selectedWeek}, ${selectedYear}!`;
+		
+		const title = `Got Games!`;
 		const msg = 'Retrieved all specified game documents from the Schedule collection.';
-		if (showToast) {
-			defaultToast({ title, msg });
-		}
-		myLog({ msg, traceLocation: true });
+		showToast ? LogAndToast({ title, msg }) : myLog({ msg, traceLocation: true });
+
 		return games;
 	} catch (error) {
 		const msg = `Encountered an error while trying to get a specific week/year of game documents.  Check the console for more info. ${error}`;
-		ErrorAndToast({ msg, error, location: 'weeklyAdmin.ts', function_name: 'getSpecificGames' });
-	}
-};
-const getMaxGameWeek = async (): Promise<number> => {
-	try {
-		const weeklyGames = await getAllGames(false);
-		const gameWeeks = weeklyGames.map((game) => {
-			return game.week;
-		});
-		const maxWeek: number = Math.max(...gameWeeks);
-		const msg = `determined the max game week was ${maxWeek}`;
-		myLog({ msg, traceLocation: true });
-		return maxWeek;
-	} catch (error) {
-		const msg = `Encountered an error while finding max game week.`;
-		ErrorAndToast({ msg, error, location: 'weeklyAdmin.ts', function_name: 'getMaxGameWeek' });
+		ErrorAndToast({ msg, error});
 	}
 };
 
@@ -92,7 +79,7 @@ export const createWeeklyPicksForAllPlayers = async () => {
 		const weeklyPlayers = await getWeeklyPlayers();
 		const games = await getAllGames();
 		for await (const player of weeklyPlayers) {
-			await createWeeklyPicksForPlayer(player, true, false, games);
+			await createWeeklyPicksForPlayer({player, games});
 		}
 		const title = 'Created Weekly Picks!';
 		const msg = 'Pick documents were created for every game, for every Weekly pool player.';
@@ -105,30 +92,24 @@ export const createWeeklyPicksForAllPlayers = async () => {
 		const msg = `Encountered an error while trying to create all players' picks.  Check the console for more info. ${error}`;
 		ErrorAndToast({
 			msg,
-			error,
-			location: 'weeklyAdmin.ts',
-			function_name: 'createWeeklyPicksForPlayer'
+			error
 		});
 	}
 };
-
+type createWeeklyPicksForPlayerOptions = {
+	player: Player;
+	games: Game[];
+	showToast?: boolean;
+	logAll?: boolean;
+}
 //TODO: Move to a triggered Cloud Function when a player joins the Weekly pool
 export const createWeeklyPicksForPlayer = async (
-	player: Player,
-	logAll = true,
-	showToast = true,
-	games?: Game[],
-	selectedWeek?: number,
-	selectedYear?: number
+	input: createWeeklyPicksForPlayerOptions
 ) => {
+	const { player, games, showToast, logAll } = input;
 	try {
-		if (!games && !selectedWeek && !selectedYear) {
-			games = await getAllGames(showToast);
-		} else if (!games && selectedWeek && selectedYear) {
-			games = await getSpecificGames(selectedWeek, selectedYear);
-		}
 		for await (const game of games) {
-			const newWeeklyPickRef = doc(weeklyPicksCollection);
+			const newWeeklyPickRef = doc(weeklyPicksCollection);  // auto-generates a new doc ref/id
 			const pickDoc = new WeeklyPickDoc({
 				docRef: newWeeklyPickRef,
 				gameId: game.id,
@@ -146,24 +127,16 @@ export const createWeeklyPicksForPlayer = async (
 		}
 		const title = 'Created Weekly Picks!';
 		const msg = `Pick documents were created for every game for ${player.name} (${player.nickname})`;
-		if (logAll) {
-			myLog({ msg, traceLocation: true });
-		}
-		if (showToast) {
-			defaultToast({ title, msg });
-		}
+		
+		if (logAll) myLog({ msg, traceLocation: true });
+		if (showToast)	defaultToast({ title, msg });
+
 	} catch (error) {
 		const msg = `Encountered an error while trying to create ${player.name}'s picks.  Check the console for more info. ${error}`;
-		if (logAll) {
 			ErrorAndToast({
 				msg,
-				error,
-				location: 'weeklyAdmin.ts',
-				function_name: 'createWeeklyPicksForPlayer'
+				error
 			});
-		} else {
-			throw error;
-		}
 	}
 };
 
@@ -185,9 +158,7 @@ export const deleteWeeklyPicksForAllPlayers = async () => {
 		const msg = `Encountered an error while trying to delete all players's picks.  Check the console for more info. ${error}`;
 		ErrorAndToast({
 			msg,
-			error,
-			location: 'weeklyAdmin.ts',
-			function_name: 'deleteWeeklyPicksForAllPlayers'
+			error
 		});
 	}
 };
@@ -225,17 +196,15 @@ export const deleteWeeklyPicksForPlayer = async (
 		const msg = `Encountered an error while trying to delete ${player.name}'s picks. Check the console for more info. ${error}`;
 		ErrorAndToast({
 			msg,
-			error,
-			location: 'weeklyAdmin.ts',
-			function_name: 'deleteWeeklyPicksForPlayer'
+			error
 		});
 	}
 };
-export const createTiebreakersForAllPlayers = async () => {
+export const createWeeklyTiebreakersForAllPlayers = async (season : SeasonBoundDoc) => {
 	try {
 		const weeklyPlayers = await getWeeklyPlayers();
 		weeklyPlayers.forEach((player) => {
-			createTiebreakersForPlayer(player, undefined, undefined, false);
+			createWeeklyTiebreakersForPlayer({player, season});
 		});
 		const title = 'Created Tiebreakers!';
 		const msg = 'Tiebreaker documents were created for every game, for every Weekly pool player.';
@@ -248,43 +217,34 @@ export const createTiebreakersForAllPlayers = async () => {
 		const msg = `Encountered an error while trying to create all players' tiebreaker documents.  Check the console for more info. ${error}`;
 		ErrorAndToast({
 			msg,
-			error,
-			location: 'weeklyAdmin.ts',
-			function_name: 'createTiebreakersForAllPlayers'
+			error
 		});
 	}
 };
-export const createTiebreakersForPlayer = async (
-	player: Player,
-	selectedWeek?: number,
-	selectedYear?: number,
-	logAll = true
+type createWeeklyTiebreakersForPlayerOptions = {
+	player: Player;
+	season: SeasonBoundDoc;
+}
+// TODO: Move to cloud function performed when player joins the pool
+export const createWeeklyTiebreakersForPlayer = async (
+	input: createWeeklyTiebreakersForPlayerOptions
 ) => {
+	const { player, season } = input;
 	try {
-		const uid = player.uid;
-		if (!selectedYear) {
-			selectedYear = new Date().getFullYear();
-		}
-		if (!selectedWeek) {
-			const maxWeek = await getMaxGameWeek();
-			for await (const week of Array(maxWeek).keys()) {
-				const selectedWeek = week;
-				await setNewTiebreakerDoc(uid, selectedWeek, selectedYear);
-			}
-		} else {
-			await setNewTiebreakerDoc(uid, selectedWeek, selectedYear);
-		}
-
-		if (logAll) {
-			myLog({ msg: `set tiebreaker for ${player.name} (${player.nickname})` });
-		}
+		const {uid} = player;
+		const {year, number_of_weeks} = season;
+		// Make an array from the number of weeks, starting with 1
+		const weeks = makeNumericArrayOfDesiredLength(number_of_weeks);
+		// Create a tiebreaker doc for each week
+		weeks.forEach(async (week) => {
+			await setNewTiebreakerDoc(uid, week, year);
+		})
+		myLog({ msg: `set tiebreakers for ${player.name} (${player.nickname})` });
 	} catch (error) {
 		const msg = `Encountered an error while trying to create tiebreaker documents for ${player.name} (${player.nickname})`;
 		ErrorAndToast({
 			msg,
-			error,
-			location: 'weeklyAdmin.ts',
-			function_name: 'createTiebreakersForPlayer'
+			error
 		});
 	}
 };
@@ -312,8 +272,6 @@ export const setNewTiebreakerDoc = async (
 		ErrorAndToast({
 			msg: `Encountered an error while trying to set tiebreaker doc (${docRef})`,
 			error,
-			location: 'weeklyAdmin.ts',
-			function_name: 'setNewTiebreakerDoc',
 			additional_params: `unable to update tiebreaker ${docRef.path} for player ${uid}`
 		});
 	}
@@ -341,9 +299,7 @@ export const deleteTiebreakersForAllPlayers = async () => {
 		const msg = `Encountered an error while trying to delete all players's tiebreakers.  Check the console for more info. ${error}`;
 		ErrorAndToast({
 			msg,
-			error,
-			location: 'weeklyAdmin.ts',
-			function_name: 'deleteTiebreakersForAllPlayers'
+			error
 		});
 	}
 };
@@ -386,9 +342,7 @@ export const deleteTiebreakersForPlayer = async (
 		const msg = `Encountered an error while trying to delete ${player.name}'s tiebreakers.  Check the console for more info. ${error}`;
 		ErrorAndToast({
 			msg,
-			error,
-			location: 'weeklyAdmin.ts',
-			function_name: 'deleteTiebreakersForPlayer'
+			error
 		});
 	}
 };
@@ -418,18 +372,6 @@ export const updateGameSpreads = async (week: number, year: number) => {
 		});
 	} catch (error) {
 		const msg = `Encountered an error while trying to update spreads for week ${week}.  Check the console for more info.`;
-		ErrorAndToast({ msg, error, location: 'weeklyAdmin.ts', function_name: 'updateGameSpreads' });
+		ErrorAndToast({ msg, error});
 	}
 };
-
-// export const fixAllTiebreakers = async () => {
-// 	const q = query(weeklyTiebreakersCollection);
-// 	const tiebreakerDocs = await getDocs(q.withConverter(weeklyTiebreakerConverter));
-// 	for await (const doc of tiebreakerDocs.docs) {
-// 		const ref = doc.ref;
-// 		const data = doc.data();
-
-// 		// Do stuff
-// 		alert('thankfully, nothing happened');
-// 	}
-// };
