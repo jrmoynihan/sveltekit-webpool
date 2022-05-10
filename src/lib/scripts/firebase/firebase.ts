d048ecc84d9fcf6a7636e2176322b28b0125dbe2
jrmoynihan <jrmoynihan@gmail.com>
2022-05-10 01:58:37 -0400

    string interpolation req'd for env var!!!

--- src/lib/scripts/firebase/firebase.ts
+++ src/lib/scripts/firebase/firebase.ts
@@ -2,9 +2,9 @@ import { getApps, getApp, initializeApp, type FirebaseApp } from '@firebase/app'
 import { getAuth, type Auth } from '@firebase/auth';
 import { Firestore, getFirestore } from '@firebase/firestore';
 
-const API_KEY: string = import.meta.env.PROD ? process.env.FIREBASE_API_KEY : import.meta.env.API_KEY as string;
+const API_KEY: string = import.meta.env.DEV ? import.meta.env.API_KEY as string : process.env.FIREBASE_API_KEY as string;
 const firebaseConfig = {
-	apiKey: `${process.env.API_KEY}`,
+	apiKey: `${API_KEY}`, // NOTE: Must leave string interpolation for SSR build to turn the env var into a string
 	authDomain: 'tonyswebpool.firebaseapp.com',
 	databaseURL: 'https://tonyswebpool.firebaseio.com',
 	projectId: 'tonyswebpool',
