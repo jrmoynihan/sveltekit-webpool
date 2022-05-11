16db98fc2bc6db8c57266a90f53bb8df41b848b4
James Moynihan <jrmoynihan@gmail.com>
2022-05-10 22:05:38 -0400

    removed comment. one day I will get env to work..

--- src/lib/scripts/firebase/firebase.ts
+++ src/lib/scripts/firebase/firebase.ts
@@ -1,8 +1,9 @@
+import { dev } from '$app/env';
 import { getApps, getApp, initializeApp, type FirebaseApp } from '@firebase/app';
 import { getAuth, type Auth } from '@firebase/auth';
 import { Firestore, getFirestore } from '@firebase/firestore';
 
-const API_KEY: string = import.meta.env.DEV ? import.meta.env.API_KEY as string : process.env.API_KEY as string;
+const API_KEY: string = dev ? import.meta.env.API_KEY as string : process.env.API_KEY as string;
 const firebaseConfig = {
 	apiKey: 'AIzaSyDEAAXuJcftdIqBRxi_OmDYmFEMs2qnpIw', // FIXME - why isn't Vercel processing this?
 	authDomain: 'tonyswebpool.firebaseapp.com',
@@ -13,13 +14,13 @@ const firebaseConfig = {
 	appId: '1:1009472673898:web:349b4dad1bccd89d2e9213',
 	measurementId: 'G-8Y5HV7HDFZ'
 };
-console.log('test env var',`${process.env.TEST}`)
 
 export function initializeFirebaseApp(): FirebaseApp {
 	// If a firebase app is already initialized, use that one
 	if (getApps().length === 0) {
 		return initializeApp(firebaseConfig);
-	} else {
+	} 
+	else {
 		return getApp();
 	}
 }
