44a1c02c17e37160158563b0018d0bfca919aa3b
James Moynihan <jrmoynihan@gmail.com>
2022-05-13 23:12:10 -0400

    feat(Functionality): :sparkles: Simplified the function to get/sort a player's weekly pool winnings.
    New DB schema makes this MUCH less hacky to do.  Simple constraints on a top-level collection query.  Still not able to use top-level await in reactive statement...

--- src/routes/admin/manage-players.svelte
+++ src/routes/admin/manage-players.svelte
@@ -5,37 +5,22 @@
 	import PlayerSelect from '$lib/components/selects/PlayerSelect.svelte';
 	import RoleToggle from '$lib/components/switches/RoleToggle.svelte';
 	import ToggleSwitch from '$lib/components/switches/ToggleSwitch.svelte';
+	import type { PlayerRecord } from '$lib/scripts/classes/playerRecord';
 	import { selected_player, updatePlayer } from '$lib/scripts/store';
-	import { onMount } from 'svelte';
-
-	let nicknameUpdate: string;
-	// let amountOwedUpdate: number;
-	// let amountPaidUpdate: number;
+	import { getWeeklyRecordData } from '$lib/scripts/weekly/weeklyPlayers';
+	import { orderBy, where } from '@firebase/firestore';
 
 	const gridStyles = 'max-width: min(100%,40rem);';
-	let weeklyWinnings: { week: string; amount: number }[];
+	let weeklyWinnings: PlayerRecord[];
 
-	const sortWeeklyWinnings = () => {
-		const keys = Object.keys($selected_player.weeklyWinnings).sort();
-		const filteredKeys = keys.filter((key) => key.match('week_'));
-		const truncatedKeys = filteredKeys.map((key) => {
-			const splitKey = key.split('_');
-			return splitKey[1];
-		});
-		truncatedKeys.sort((first, second) => Number(first) - Number(second));
-		const sorted = truncatedKeys.map((key) => {
-			return { week: key, amount: $selected_player.weeklyWinnings[`week_${key}`] };
-		});
-		return sorted;
+	const getPlayerWeeklyWinnings = async (player) => {
+		const constraints = [where('uid', '==', player.uid), orderBy('week'), orderBy('year')];
+		const winnings = await getWeeklyRecordData({ constraints });
+		return winnings;
 	};
-	const updateDisplayedPlayer = (): void => {
-		weeklyWinnings = sortWeeklyWinnings();
-		nicknameUpdate = $selected_player.nickname;
-	};
-
-	onMount(async () => {
-		nicknameUpdate = $selected_player.nickname;
-		weeklyWinnings = sortWeeklyWinnings();
+	// TODO: Can this be made into a top-level await somehow?
+	$: getPlayerWeeklyWinnings($selected_player).then((winnings) => {
+		weeklyWinnings = winnings;
 	});
 </script>
 
@@ -44,11 +29,7 @@
 <Grid
 	customStyles="{gridStyles} grid-template-columns: auto max-content;text-align:left;justify-self:center;"
 >
-	<PlayerSelect
-		player_pool="all"
-		on:change={updateDisplayedPlayer}
-		customStyles="grid-column:span 2"
-	/>
+	<PlayerSelect player_pool="all" customStyles="grid-column:span 2" />
 	<p class="title">Name:</p>
 	<p>{$selected_player.name}</p>
 	<p class="title">Email:</p>
@@ -56,7 +37,7 @@
 	<p class="title">UserID:</p>
 	<p>{$selected_player.uid}</p>
 	<p class="title">Nickname:</p>
-	<input type="text" bind:value={nicknameUpdate} />
+	<input type="text" bind:value={$selected_player.nickname} />
 </Grid>
 <hr />
 <Grid>
@@ -70,27 +51,27 @@
 <Grid>
 	<ToggleSwitch
 		on:toggle={async () => await updatePlayer($selected_player)}
-		bind:checked={$selected_player.paidCollege}
+		bind:checked={$selected_player.paid_college}
 		labelText="Paid College"
 	/>
 	<ToggleSwitch
 		on:toggle={async () => await updatePlayer($selected_player)}
-		bind:checked={$selected_player.paidPick6}
+		bind:checked={$selected_player.paid_pick6}
 		labelText="Paid Pick6"
 	/>
 	<ToggleSwitch
 		on:toggle={async () => await updatePlayer($selected_player)}
-		bind:checked={$selected_player.paidPlayoffs}
+		bind:checked={$selected_player.paid_playoffs}
 		labelText="Paid Playoffs"
 	/>
 	<ToggleSwitch
 		on:toggle={async () => await updatePlayer($selected_player)}
-		bind:checked={$selected_player.paidSurvivor}
+		bind:checked={$selected_player.paid_survivor}
 		labelText="Paid Survivor"
 	/>
 	<ToggleSwitch
 		on:toggle={async () => await updatePlayer($selected_player)}
-		bind:checked={$selected_player.paidWeekly}
+		bind:checked={$selected_player.paid_weekly}
 		labelText="Paid Weekly"
 	/>
 </Grid>
@@ -99,21 +80,21 @@
 		grid-template-columns: auto 1fr;"
 >
 	<label for="amountOwed" class="title"> Amount Owed to Pool </label>
-	<input id="amountOwed" type="number" value={$selected_player.amountOwedToPools} />
+	<input id="amountOwed" type="number" value={$selected_player.amount_owed_to_pools} />
 
 	<label for="amountPaid" class="title"> Amount Paid by Pool </label>
-	<input id="amountPaid" type="number" value={$selected_player.amountPaidToPools} />
+	<input id="amountPaid" type="number" value={$selected_player.amount_paid_to_pools} />
 
 	<label for="winningsSummary" class="title"> Weekly Winnings </label>
 	<AccordionDetails expanderIconSide={'none'}>
 		<div id="winningsSummary" slot="summary">
-			<p class="title">${$selected_player.weeklyWinnings.total} total</p>
+			<p class="title">${$selected_player.weekly_pool_season_winnings} total</p>
 			<p><small><i>(click to reveal breakdown)</i></small></p>
 		</div>
 		<div slot="content">
 			{#if weeklyWinnings}
 				{#each weeklyWinnings as winningWeek}
-					<p>Week {winningWeek.week}: ${winningWeek.amount}</p>
+					<p>Week {winningWeek.week}: ${winningWeek.prize_amount}</p>
 				{/each}
 			{/if}
 		</div>
