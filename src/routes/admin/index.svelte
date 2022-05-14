86993c423a7c8b54a9b03520ca94ff9265c1bc6e
James Moynihan <jrmoynihan@gmail.com>
2022-05-13 23:09:03 -0400

    open newplayerform binding exposed

--- src/routes/admin/index.svelte
+++ src/routes/admin/index.svelte
@@ -2,19 +2,18 @@
 	import Grid from '$lib/components/containers/Grid.svelte';
 	import NewPlayerForm from '$lib/components/forms/NewPlayerForm.svelte';
 	import PageTitle from '$lib/components/misc/PageTitle.svelte';
-	import type ModalOnly from '$lib/components/modals/Modal.svelte';
 	import { toastIt } from '$scripts/toasts';
 
-	let modalOnlyComponent: ModalOnly;
+	let openNewPlayerForm: () => Promise<void>;
 </script>
 
 <PageTitle>Admin</PageTitle>
 <svelte:head>
 	<title>Admin</title>
 </svelte:head>
-<NewPlayerForm bind:modalOnlyComponent />
+<NewPlayerForm bind:openNewPlayerForm />
 <Grid>
-	<button on:click={() => modalOnlyComponent.open()}>Open New Player Form</button>
+	<button on:click={openNewPlayerForm}>Open New Player Form</button>
 	<button on:click={() => toastIt('Test Toast!', `Here's to you, bold adventurer!`)}
 		>Toast It!</button
 	>
