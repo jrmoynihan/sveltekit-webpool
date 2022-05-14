730ebd646b6a9420b62d3d2522d7b500cf378fb6
James Moynihan <jrmoynihan@gmail.com>
2022-05-13 23:07:44 -0400

    refactor(Components): :truck: Moved admin tabs to site.ts
    Locates site / url path-specific variables all in one place.

--- src/routes/admin/__layout.svelte
+++ src/routes/admin/__layout.svelte
@@ -9,19 +9,11 @@
 <script lang="ts">
 	import NavLink from '$navigation/NavLink.svelte';
 	import Navigator from '$navigation/Navigator.svelte';
-	import { PageOption } from '$scripts/classes/pageOption';
 	import TransitionWrapper from '$lib/components/TransitionWrapper.svelte';
 	import type { LoadInput, LoadOutput } from '@sveltejs/kit';
+	import { admin_tabs } from '$lib/scripts/site';
 
 	export let refresh: unknown;
-
-	const adminTabs: PageOption[] = [
-		new PageOption({ navigationText: 'Manage Players', path: '/admin/manage-players' }),
-		new PageOption({ navigationText: 'Manage Teams', path: '/admin/manage-teams' }),
-		new PageOption({ navigationText: 'Game Data', path: '/admin/set-games' }),
-		new PageOption({ navigationText: 'Weekly Pool Admin', path: '/admin/weekly-pool-admin' }),
-		new PageOption({ navigationText: 'The Sandbox', path: '/admin/sandbox' })
-	];
 
 	const maxLengthText: number = 10;
 	// Can change 'maxLengthText = Math.max(...textLengths)' to keep all text in a single line
@@ -31,8 +23,8 @@
 </script>
 
 <Navigator minItemSize="{maxLengthText}ch" maxItemSize="max-content" customStyles="top:3.2rem;">
-	{#each adminTabs as tab, index}
-		<NavLink {index} pageOption={tab} fullyRounded={true} />
+	{#each admin_tabs as tab, index}
+		<NavLink {index} page_option={tab} fully_rounded={true} />
 	{/each}
 </Navigator>
 <TransitionWrapper {refresh} customStyles="display:grid;">
