bc40c30195e63e035bced460bc17fd683a1e18f9
James Moynihan <jrmoynihan@gmail.com>
2022-05-13 23:05:10 -0400

    feat(Auth): :passport_control: Bound new player form to this layout. Updated stores to reactive functions.
    Making even more use of stores globally. Will now update user/player stores upon login and open new player form, if necessary.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -18,6 +18,7 @@
 <script lang="ts">
 	import '../app.css';
 	import {
+		all_players,
 		current_player,
 		current_season,
 		firebase_user,
@@ -34,20 +35,19 @@
 	import SiteNavOptions from '$navigation/siteNavOptions.svelte';
 	import ReturnToTop from '$lib/components/buttons/ReturnToTop.svelte';
 	import { SvelteToast } from '@zerodevx/svelte-toast';
-	import { mobileBreakpoint } from '$scripts/site';
+	import { mobile_breakpoint } from '$scripts/site';
 	import { onMount } from 'svelte';
 	import { getLocalStorageItem } from '$scripts/localStorage';
 	import NewPlayerForm from '$lib/components/forms/NewPlayerForm.svelte';
-	import type ModalOnly from '$lib/components/modals/Modal.svelte';
 	import type { LoadInput, LoadOutput } from '@sveltejs/kit';
 	import { get } from 'svelte/store';
 	import { findCurrentSeason } from '$lib/scripts/schedule';
-	import { getPlayer } from '$lib/scripts/weekly/weeklyPlayers';
+	import { myLog } from '$lib/scripts/logging';
 
 	export let refresh: any;
-	let modalOnlyComponent: ModalOnly;
+	let openNewPlayerForm: () => Promise<void> = async () => {};
 	const checkWindowWidth = () => {
-		if ($window_width > mobileBreakpoint) {
+		if ($window_width > mobile_breakpoint) {
 			$larger_than_mobile = true;
 		} else {
 			$larger_than_mobile = false;
@@ -60,12 +60,25 @@
 		}
 	};
 	const setStores = async () => {
-		$current_player = await getPlayer($firebase_user);
-		$selected_player = $current_player;
-		$current_season = await findCurrentSeason();
-		$selected_season = $current_season;
+		if ($firebase_user) {
+			$current_player = $all_players.find((player) => player.uid === $firebase_user.uid);
+			$selected_player = $current_player;
+
+			if ($current_player) {
+				myLog({ msg: `Player found: ${$current_player.name} (${$current_player.nickname})` });
+			} else {
+				openNewPlayerForm();
+				myLog({ msg: `Player NOT found for user: ${$firebase_user?.displayName}` });
+			}
+			$current_season = await findCurrentSeason();
+			$selected_season = $current_season;
+		}
 	};
-	$: if ($firebase_user) setStores();
+	// Update the current player whenever their document has changes
+	$: $current_player = $all_players.find((player) => player.uid === $firebase_user.uid);
+
+	// After the players are loaded in the onSnapshot listener, we can set the current/selected player
+	$: if ($all_players) setStores();
 
 	onMount(async () => {
 		checkWindowWidth();
@@ -92,7 +105,7 @@
 		>
 			<slot />
 		</TransitionWrapper>
-		<NewPlayerForm bind:modalOnlyComponent />
+		<NewPlayerForm bind:openNewPlayerForm />
 	</main>
 
 	<ReturnToTop showButton={false} />
