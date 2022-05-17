37fefbbeec324c4d090ff995bb924213c4aeb006
James Moynihan <jrmoynihan@gmail.com>
2022-05-16 21:53:15 -0400

    refactor(Components): :art: Removed placeholder slots. Moved expand text into slot and separated into two rows. Changed grid layout of team buttons. Exposed toggle function and open state of accordion directly.
    Placeholder slots were redundant UI; Streamlined into a single place for picks to appear. Second row of text makes it easier to read the pick count. Team buttons were bigger than necessary, so new layout fits more on screen. Better automatic opening/closing of accordions is now enabled.

--- src/lib/components/containers/accordions/PickSixGroup.svelte
+++ src/lib/components/containers/accordions/PickSixGroup.svelte
@@ -1,19 +1,16 @@
 <script lang="ts">
-	import Grid from '../Grid.svelte';
 	import AccordionDetails from './AccordionDetails.svelte';
 	import { quintOut } from 'svelte/easing';
 	import { crossfade } from 'svelte/transition';
 	import type { pickSixItem } from '$scripts/types/types';
 	import { flip } from 'svelte/animate';
-	import { larger_than_mobile } from '$scripts/store';
 	import PickSixButton from '$lib/components/buttons/PickSixButton.svelte';
 
 	export let group: pickSixItem[];
-	export let groupSelectedCount: number;
-	export let groupLetter: string;
-	let accordion: AccordionDetails;
-	let open: boolean;
-	let toggle: () => boolean;
+	export let group_selected_count: number;
+	export let group_letter: string;
+	export let toggle: () => boolean;
+	export let open: boolean;
 
 	const [send, receive] = crossfade({
 		duration: (d) => Math.sqrt(d * 200),
@@ -32,66 +29,31 @@
 			};
 		}
 	});
-	$: if (groupSelectedCount === 2) setTimeout(() => accordion?.toggle(), 500);
 </script>
 
 <AccordionDetails
-	bind:this={accordion}
 	bind:toggle
 	bind:open
 	expanderIconSide={'left'}
-	expandTitle="Group {groupLetter} ({groupSelectedCount}/2 picks made)"
-	customSummaryStyles="font-weight:bold; {groupSelectedCount === 2
+	customSummaryStyles="font-weight:bold; {group_selected_count === 2
 		? 'background: hsla(var(--accent-value), 40%); color:var(--text);'
 		: ''} transition: all 300ms ease-in-out; "
 >
-	<div
-		slot="content"
-		class="pick6 grid"
-		style="grid-template-columns: {$larger_than_mobile ? '1fr 1fr' : '1fr'}"
-	>
-		<Grid
-			minColumns={'25%'}
-			maxColumns={'1fr'}
-			customStyles={'align-items:start; grid-template-rows: repeat(3,minmax(5rem,35%));'}
-		>
-			{#each group.filter((item) => !item.selected) as teamOption (teamOption.team.abbreviation)}
-				<div
-					class="animation-container"
-					animate:flip={{ duration: 300 }}
-					in:receive={{ key: teamOption.team.abbreviation }}
-					out:send={{ key: teamOption.team.abbreviation }}
-				>
-					<PickSixButton bind:teamOption bind:selectedCount={groupSelectedCount} />
-				</div>
-			{/each}
-		</Grid>
-		{#if $larger_than_mobile}
-			<Grid
-				minColumns={'25%'}
-				maxColumns={'49%'}
-				customStyles={`align-items:start; grid-template-rows: repeat(3,minmax(0,1fr)); grid-template-columns: ${
-					$larger_than_mobile ? '1fr 1fr' : '1fr'
-				};`}
+	<div slot="summary">
+		<p>Group {group_letter}</p>
+		<p>({group_selected_count}/2 picks made)</p>
+	</div>
+	<div slot="content" class="pick6 grid condensed expansive">
+		{#each group.filter((item) => !item.selected) as teamOption (teamOption.team.abbreviation)}
+			<div
+				class="animation-container"
+				animate:flip={{ duration: 300 }}
+				in:receive={{ key: teamOption.team.abbreviation }}
+				out:send={{ key: teamOption.team.abbreviation }}
 			>
-				{#each group.filter((item) => item.selected) as teamOption (teamOption.team.abbreviation)}
-					<div
-						class="animation-container"
-						animate:flip={{ duration: 300 }}
-						in:receive={{ key: teamOption.team.abbreviation }}
-						out:send={{ key: teamOption.team.abbreviation }}
-					>
-						<PickSixButton bind:teamOption bind:selectedCount={groupSelectedCount} />
-					</div>
-				{/each}
-				{#if groupSelectedCount === 0}
-					<div class="placeholder">Pick 1</div>
-					<div class="placeholder">Pick 2</div>
-				{:else if groupSelectedCount === 1}
-					<div class="placeholder">Pick 2</div>
-				{/if}
-			</Grid>
-		{/if}
+				<PickSixButton bind:teamOption bind:selectedCount={group_selected_count} />
+			</div>
+		{/each}
 	</div>
 </AccordionDetails>
 
@@ -99,18 +61,24 @@
 	.grid {
 		@include gridAndGap;
 		padding: 1rem;
-		@include responsive_mobile_only {
-			grid-template-rows: repeat(auto-fit, minmax(0, 1fr));
+		grid-template-rows: repeat(auto-fit, minmax(1fr, auto));
+	}
+	.condensed {
+		grid-template-columns: 1fr 1fr;
+	}
+	.expansive {
+		@include responsive_desktop_only {
+			grid-template-columns: repeat(4, 1fr);
 		}
 	}
 	.animation-container {
 		height: 100%;
 	}
-	.placeholder {
-		@include rounded;
-		display: grid;
-		place-content: center;
-		outline: 2px var(--accent) solid;
-		height: 10rem;
-	}
+	// .placeholder {
+	// 	@include rounded;
+	// 	display: grid;
+	// 	place-content: center;
+	// 	outline: 2px var(--accent) solid;
+	// 	height: 10rem;
+	// }
 </style>
