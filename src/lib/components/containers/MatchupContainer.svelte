0ba067ecc5c3483ce579ddebea767622d5de535d
James Moynihan <jrmoynihan@gmail.com>
2022-05-20 07:32:39 -0400

    refactor(Components): :recycle: Changed to snake_casing. Simplified most reactive statements.
    Disabled state is much easier to understand now, as is when team name images are shown. Could move that to CSS only selectors instead.

--- src/lib/components/containers/MatchupContainer.svelte
+++ src/lib/components/containers/MatchupContainer.svelte
@@ -13,72 +13,57 @@
 	export let id = 'id';
 	export let index: number;
 	export let spread = 0;
-	export let homeTeam: Team;
-	export let awayTeam: Team;
+	export let home_team: Team;
+	export let away_team: Team;
 	export let timestamp: Timestamp;
-	export let selectedTeam = '';
+	export let selected_team_abbreviation = '';
 	export let competitions = [];
-	export let currentPicks: WeeklyPickDoc[] = [];
-	export let gridColumns = 1;
-	export let isATSwinner: null | boolean = null;
+	export let current_picks: WeeklyPickDoc[] = [];
+	export let grid_columns = 1;
+	export let is_ATS_winner: null | boolean = null;
 	export let ATS_winner: string;
-	export let beforeGameTime = false;
-	let layoutBreakpoint = 620;
-	let showTeamNameImages = false;
-	let disabled = !beforeGameTime;
-	let gameIsOver = false;
+	export let is_before_game_time = false;
+	let layout_breakpoint = 620;
+	let show_team_name_images = false;
+	let disabled: boolean;
+	let game_is_over = false;
 	let element: HTMLElement;
-	let showGameContainer = false;
+	let show_game_container = false;
 
-	$: {
-		if ($overrideDisabled) {
-			disabled = false;
-		} else {
-			checkGameTime();
-		}
-	}
+	$: disabled = $overrideDisabled || !is_before_game_time;
 
 	const checkGameTime = async () => {
 		if (await isBeforeGameTime(timestamp)) {
-			disabled = false;
-			beforeGameTime = true;
-			// myLog(`game ${id} hasn't started`);
+			is_before_game_time = true;
 		} else {
-			disabled = true;
-			beforeGameTime = false;
-			// myLog(`game ${id} has started`);
+			is_before_game_time = false;
 			clearInterval(recheckGameTimeInterval);
 		}
 	};
 
-	let promiseStatus: Promise<ESPNStatus> = getStatus(competitions);
-	let promiseScores: Promise<{ homeScoreData: ESPNScore; awayScoreData: ESPNScore }> =
+	let promise_status: Promise<ESPNStatus> = getStatus(competitions);
+	let promise_scores: Promise<{ homeScoreData: ESPNScore; awayScoreData: ESPNScore }> =
 		getScores(competitions);
-	let promiseSituation: Promise<ESPNSituation> = getSituation(competitions);
+	let promise_situation: Promise<ESPNSituation> = getSituation(competitions);
 	let statusInterval: NodeJS.Timer;
 	let recheckGameTimeInterval: NodeJS.Timer;
 
 	// Show additional images in larger layout sizes
-	$: {
-		if ($window_width < layoutBreakpoint * gridColumns) {
-			showTeamNameImages = false;
-		} else {
-			showTeamNameImages = true;
-		}
-	}
+	$: show_team_name_images = $window_width > layout_breakpoint * grid_columns;
+
 	// ******************
 	// Periodically re-check if the game is over
-	$: promiseStatus.then(async (status) => {
+	$: promise_status.then(async (status) => {
 		if (status?.type?.description === 'Final') {
-			gameIsOver = true;
-			beforeGameTime = false;
+			game_is_over = true;
+			is_before_game_time = false;
 		} else if (status?.type?.description === 'Scheduled') {
-			gameIsOver = false;
-			beforeGameTime = true;
+			game_is_over = false;
+			is_before_game_time = true;
 		}
 	});
 	// If the game isn't active, stop calling for status data, preventing unncessary API calls
-	$: if (gameIsOver || beforeGameTime) {
+	$: if (game_is_over || is_before_game_time) {
 		clearInterval(statusInterval);
 	}
 	// *****************
@@ -97,9 +82,9 @@
 		// Every 60 seconds, update the game status if the game has started, but hasn't ended
 		statusInterval = setInterval(() => {
 			if (competitions) {
-				promiseScores = getScores(competitions);
-				promiseStatus = getStatus(competitions);
-				promiseSituation = getSituation(competitions);
+				promise_scores = getScores(competitions);
+				promise_status = getStatus(competitions);
+				promise_situation = getSituation(competitions);
 			}
 		}, 60_000);
 	});
@@ -113,38 +98,38 @@
 
 <div bind:this={element} id="game-{index}">
 	<TeamSelectRadioInput
-		bind:homeOrAwayTeam={awayTeam}
+		bind:team={away_team}
 		bind:id
-		bind:selectedTeam
-		bind:currentPicks
+		bind:selected_team_abbreviation
+		bind:current_picks
 		bind:element
-		bind:showGameContainer
-		bind:showTeamNameImages
+		bind:show_game_container
+		bind:show_team_name_images
 		bind:disabled
 	/>
 	<GameInfo
 		bind:id
-		bind:selectedTeam
+		bind:selected_team_abbreviation
 		bind:timestamp
-		bind:homeTeam
-		bind:awayTeam
+		bind:home_team
+		bind:away_team
 		bind:spread
 		bind:disabled
-		bind:promiseScores
-		bind:promiseStatus
-		bind:promiseSituation
-		bind:isATSwinner
-		bind:ATSwinner={ATS_winner}
-		bind:gameIsOver
+		bind:promise_scores
+		bind:promise_status
+		bind:promise_situation
+		bind:is_ATS_winner
+		bind:ATS_winner
+		bind:game_is_over
 	/>
 	<TeamSelectRadioInput
-		bind:homeOrAwayTeam={homeTeam}
+		bind:team={home_team}
 		bind:id
-		bind:selectedTeam
-		bind:currentPicks
+		bind:selected_team_abbreviation
+		bind:current_picks
 		bind:element
-		bind:showGameContainer
-		bind:showTeamNameImages
+		bind:show_game_container
+		bind:show_team_name_images
 		bind:disabled
 	/>
 </div>
