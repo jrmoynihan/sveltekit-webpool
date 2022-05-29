8812275f5289427d9570e0b8da078371d568ec40
James Moynihan <jrmoynihan@gmail.com>
2022-05-29 10:34:01 -0400

    added ability to group console logs (may revert)

--- src/lib/scripts/logging.ts
+++ src/lib/scripts/logging.ts
@@ -1,18 +1,18 @@
-import { all_icons, defaultConsoleLogStyle } from "$classes/constants";
-import { type myToastOptions, defaultToast, errorToast } from "$scripts/toasts";
+import { all_icons, defaultConsoleLogStyle } from '$classes/constants';
+import { type myToastOptions, defaultToast, errorToast } from '$scripts/toasts';
 
 export interface myLogType {
 	msg: string;
 	icon?: string | null;
 	additional_params?: any;
-	traceLocation?: boolean
-};
-export interface myErrorType{
+	traceLocation?: boolean;
+}
+export interface myErrorType {
 	error: Error;
 	msg?: string;
 	icon?: string | null;
 	additional_params?: any;
-};
+}
 
 // Creates a union of the two types for combined usage
 export type LogAndToastType = myToastOptions & myLogType;
@@ -31,23 +31,25 @@ export const myLog = (input: myLogType): void => {
 	let str = icon ? `%c${icon} ` : '%c';
 	str += `\n ${msg}\ `;
 	let args = [str, defaultConsoleLogStyle];
-	
-	if(additional_params) args = args.concat(additional_params);
-	
-	if(traceLocation){
+
+	if (additional_params) args = args.concat(additional_params);
+
+	if (traceLocation) {
 		console.trace(...args);
-	}else{
-		console.log(...args)
+	} else if (additional_params) {
+		console.group(...args);
+	} else {
+		console.log(...args);
 	}
 };
 
-export const LogAndToast = (options: LogAndToastType ): number => {
-	myLog({...options});
-	const toastId = defaultToast({...options});
+export const LogAndToast = (options: LogAndToastType): number => {
+	myLog({ ...options });
+	const toastId = defaultToast({ ...options });
 	return toastId;
-}
-export const ErrorAndToast = (options: ErrorAndToastType ): number => {
-	myError({...options});
-	const toastId = errorToast({...options});
+};
+export const ErrorAndToast = (options: ErrorAndToastType): number => {
+	myError({ ...options });
+	const toastId = errorToast({ ...options });
 	return toastId;
-}
+};
