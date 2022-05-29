887220b21e6a7bd4d19f29c92a0be467ba1e0d91
James Moynihan <jrmoynihan@gmail.com>
2022-05-28 23:39:30 -0400

    perf(Media / Static Assets): :bento: Use svelte-image to create more sizes of team images
    Optimized size images will maximize quality at different screen sizes.

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
+import svelteImage from 'svelte-image';
 // import vercel from '@sveltejs/adapter-vercel';
 import path, { dirname } from 'path';
 import mkcert from 'vite-plugin-mkcert';
@@ -21,6 +22,52 @@ const config = {
 			scss: {
 				prependData: `@import '${sassPath}/mixins.scss';`,
 				includePaths: [sassPath]
+			}
+		}),
+		svelteImage({
+			sizes: [50, 100, 200, 400, 800, 1200, 1600],
+			processFolders: [
+				'ARI',
+				'ATL',
+				'BAL',
+				'BUF',
+				'CAR',
+				'CHI',
+				'CIN',
+				'CLE',
+				'DAL',
+				'DEN',
+				'DET',
+				'GB',
+				'HOU',
+				'IND',
+				'JAX',
+				'KC',
+				'LAC',
+				'LAR',
+				'LV',
+				'MIA',
+				'MIN',
+				'NE',
+				'NO',
+				'NYG',
+				'NYJ',
+				'PHI',
+				'PIT',
+				'SEA',
+				'SF',
+				'TB',
+				'TEN',
+				'WSH'
+			],
+			processFoldersRecursively: true,
+			processFoldersExtensions: ['jpg', 'jpeg', 'png', 'gif', 'svg', 'webp', 'avif'],
+			processFoldersSizes: [50, 100, 200, 400, 800, 1200, 1600],
+			outputDir: 'g/',
+			placeholder: 'blur',
+			webpOptions: {
+				quality: 95,
+				smartSubsample: true
 			}
 		})
 	],
