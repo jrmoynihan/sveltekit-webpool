45802edfee48a9be7dfd789bac5b2847c8b4b87d
James Moynihan <jrmoynihan@gmail.com>
2022-06-02 08:46:16 -0400

    refactor(Components): :art: Removed unnecessary custom on:click event handler.
    on:click events can simply be listened to by the parent, since they bubble up.

--- src/lib/components/buttons/StyledButton.svelte
+++ src/lib/components/buttons/StyledButton.svelte
@@ -1,16 +1,10 @@
 <script lang="ts">
-	import { createEventDispatcher } from 'svelte';
-
 	export let text = '';
 	export let disabled = false;
 	export let customStyles = '';
-	const dispatch = createEventDispatcher();
-	function buttonClicked(event: MouseEvent): void {
-		dispatch('click', event.detail);
-	}
 </script>
 
-<button on:click={buttonClicked} {disabled} style={customStyles}>
+<button on:click {disabled} style={customStyles}>
 	{text}
 	<slot />
 </button>
