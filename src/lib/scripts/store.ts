43cccec8d18b7d9f692db7b6d2d54e49cbbc53fd
James Moynihan <jrmoynihan@gmail.com>
2022-06-03 01:01:14 -0400

    added current_season_records store

--- src/lib/scripts/store.ts
+++ src/lib/scripts/store.ts
@@ -1,9 +1,27 @@
 import { browser } from '$app/env';
 import { type Writable, writable, derived, get } from 'svelte/store';
-import { doc, updateDoc, onSnapshot, query, type Query, type FirestoreDataConverter } from '@firebase/firestore';
-import { playerConverter, seasonBoundConverter, teamConverter } from './converters';
+import {
+	doc,
+	updateDoc,
+	onSnapshot,
+	query,
+	type Query,
+	type FirestoreDataConverter,
+	where
+} from '@firebase/firestore';
+import {
+	playerConverter,
+	seasonBoundConverter,
+	seasonRecordConverter,
+	teamConverter
+} from './converters';
 import { Player } from '$classes/player';
-import { playersCollection, seasonBoundsCollection, teamsCollection } from './collections';
+import {
+	playersCollection,
+	seasonBoundsCollection,
+	seasonRecordsCollection,
+	teamsCollection
+} from './collections';
 import type { ScoreViewPreference } from './types/types';
 import type { WeeklyTiebreaker } from './classes/tiebreaker';
 import type { WeeklyPickDoc } from './classes/picks';
@@ -12,6 +30,7 @@ import type { Team } from '$classes/team';
 import type { User } from '@firebase/auth';
 import { firebaseAuth } from './firebase/firebase';
 import type { SeasonBoundDoc } from './classes/seasonBound';
+import type { SeasonRecord } from './classes/playerRecord';
 
 export const use_dark_theme = writable(false);
 export const nav_toggled = writable(true);
@@ -27,16 +46,35 @@ export const show_timestamps = writable(false);
 export const show_ATS_winner = writable(false);
 export const preferred_score_view = writable<ScoreViewPreference>('Both');
 export const current_season = writable<SeasonBoundDoc>();
-export const current_season_type_number = derived(current_season, $currentSeason => $currentSeason?.type_number)
-export const current_season_start = derived(current_season, $currentSeason => $currentSeason?.start_date)
-export const current_season_end = derived(current_season, $currentSeason => $currentSeason?.end_date)
-export const current_season_type_name = derived(current_season, $currentSeasonType => $currentSeasonType?.type_name)
-export const current_season_year = derived(current_season, $currentSeason => parseInt($currentSeason?.doc_id))
+export const current_season_type_number = derived(
+	current_season,
+	($currentSeason) => $currentSeason?.type_number
+);
+export const current_season_start = derived(
+	current_season,
+	($currentSeason) => $currentSeason?.start_date
+);
+export const current_season_end = derived(
+	current_season,
+	($currentSeason) => $currentSeason?.end_date
+);
+export const current_season_type_name = derived(
+	current_season,
+	($currentSeasonType) => $currentSeasonType?.type_name
+);
+export const current_season_year = derived(current_season, ($currentSeason) =>
+	parseInt($currentSeason?.doc_id)
+);
 export const current_season_week = writable(1);
 export const selected_season = writable<SeasonBoundDoc>();
-export const selected_season_type = writable<string>('Regular Season')
-export const selected_season_type_number = derived(selected_season, $selectedSeason => $selectedSeason?.type_number)
-export const selected_season_year = derived(selected_season, $selectedSeason => parseInt($selectedSeason?.doc_id))
+export const selected_season_type = writable<string>('Regular Season');
+export const selected_season_type_number = derived(
+	selected_season,
+	($selectedSeason) => $selectedSeason?.type_number
+);
+export const selected_season_year = derived(selected_season, ($selectedSeason) =>
+	parseInt($selectedSeason?.doc_id)
+);
 export const selected_player = writable<Player>(new Player({}));
 export const selected_week = writable(get(current_season_week) || 1);
 export const selected_year = writable(get(current_season_year) || new Date().getFullYear());
@@ -47,7 +85,7 @@ export const tiebreaker_promise = writable<Promise<WeeklyTiebreaker[]>>();
 export const players_promise = writable<Promise<Player[]>>(new Promise<Player[]>(() => {}));
 export const override_locked_picks = writable(false);
 export const firebase_user = writable<User>(firebaseAuth.currentUser); // The firebase user. Not necessarily a player yet, and without web pool-specific data
-export const current_player = writable<Player>(new Player({}));  // Who is logged into the app; their player data
+export const current_player = writable<Player>(new Player({})); // Who is logged into the app; their player data
 export const tiebreaker_score_guess = writable(0);
 
 // export const queryAsStore = (
@@ -64,13 +102,13 @@ export const tiebreaker_score_guess = writable(0);
 export const writableQueryAsStore = (
 	query: Query,
 	converter: FirestoreDataConverter<unknown>
-) : Writable<unknown> =>
+): Writable<unknown> =>
 	writable([], (set) => {
 		const unsubscribe = onSnapshot(query.withConverter(converter), async (snap) => {
 			const data = snap.docs.map((doc) => doc.data());
 			set(data);
 		});
-		return unsubscribe
+		return unsubscribe;
 	});
 
 /**
@@ -84,7 +122,7 @@ export const writableQueryAsStore = (
 export const playerQueryAsStore = (query: Query): Writable<Player[]> =>
 	writable<Player[]>([], (set) => {
 		const unsubscribe = onSnapshot(query.withConverter(playerConverter), async (snap) => {
-			const player_data = snap.docs.map(doc => doc.data())
+			const player_data = snap.docs.map((doc) => doc.data());
 			set(player_data);
 		});
 		return unsubscribe;
@@ -115,7 +153,21 @@ export const updatePlayer = async (player: Player): Promise<void> => {
 	}
 };
 
-export const all_seasons = writableQueryAsStore(query(seasonBoundsCollection), seasonBoundConverter) as Writable<SeasonBoundDoc[]>;
-export const all_teams = writableQueryAsStore(query(teamsCollection), teamConverter) as Writable<Team[]>;
-export const all_players = writableQueryAsStore(query(playersCollection), playerConverter) as Writable<Player[]>;
-export const weekly_players = derived(all_players, $allPlayers => $allPlayers.filter(player => player.weekly))
+export const all_seasons = writableQueryAsStore(
+	query(seasonBoundsCollection),
+	seasonBoundConverter
+) as Writable<SeasonBoundDoc[]>;
+export const all_teams = writableQueryAsStore(query(teamsCollection), teamConverter) as Writable<
+	Team[]
+>;
+export const all_players = writableQueryAsStore(
+	query(playersCollection),
+	playerConverter
+) as Writable<Player[]>;
+export const weekly_players = derived(all_players, ($allPlayers) =>
+	$allPlayers.filter((player) => player.weekly)
+);
+export const current_season_records = writableQueryAsStore(
+	query(seasonRecordsCollection, where('season_year', '==', get(current_season_year))),
+	seasonRecordConverter
+) as Writable<SeasonRecord[]>;
