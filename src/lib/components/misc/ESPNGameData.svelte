02413c6325f1953c640dc639cb14def8350aed52
James Moynihan <jrmoynihan@gmail.com>
2022-06-04 09:45:49 -0400

    removed unecessary section tag

--- src/lib/components/misc/ESPNGameData.svelte
+++ src/lib/components/misc/ESPNGameData.svelte
@@ -9,26 +9,24 @@
 	export let maxHeight = 25;
 </script>
 
-<section>
-	{#if title}
-		<p>{title}</p>
-	{/if}
-	{#each gameData as game, i (game.id)}
-		<AccordionDetails
-			expanderIconSide={'left'}
-			cloudyBackground={false}
-			expandTitle={`Game ${i + 1} -- ${game.date.substring(0, 10)} ${game.name}`}
-			customSummaryStyles="padding:1rem;"
-			customContentStyles={`max-height: ${maxHeight}vh; scrollbar-width: thin;`}
-		>
-			<svelte:fragment slot="content">
-				{#each Object.entries(game) as [key, value] (key)}
-					<AccordionJson {key} {value} />
-				{/each}
-			</svelte:fragment>
-		</AccordionDetails>
-	{/each}
-</section>
+{#if title}
+	<p>{title}</p>
+{/if}
+{#each gameData as game, i (game.id)}
+	<AccordionDetails
+		expanderIconSide={'left'}
+		cloudyBackground={false}
+		expandTitle={`Game ${i + 1} -- ${game.date.substring(0, 10)} ${game.name}`}
+		customSummaryStyles="padding:1rem;"
+		customContentStyles={`max-height: ${maxHeight}vh; scrollbar-width: thin;`}
+	>
+		<svelte:fragment slot="content">
+			{#each Object.entries(game) as [key, value] (key)}
+				<AccordionJson {key} {value} />
+			{/each}
+		</svelte:fragment>
+	</AccordionDetails>
+{/each}
 
 <style lang="scss">
 	p {
