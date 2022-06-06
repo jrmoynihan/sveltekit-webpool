d434c46c230d3a05e580952282a24b7966e2b63e
James Moynihan <jrmoynihan@gmail.com>
2022-06-05 22:21:11 -0400

    fix(Components): :adhesive_bandage: Updated context variables to be properly typed as Writable<Promise<T>>
    Context variables have a major shortcoming: they don't know the type at compile time without explicit typing.  This correctly assigns them the right types and accesses the stores with $ prefixes.

--- src/lib/components/containers/micro/ScoresATS.svelte
+++ src/lib/components/containers/micro/ScoresATS.svelte
@@ -2,21 +2,23 @@
 	import type { ESPNScore, ESPNStatus } from '$scripts/classes/game';
 	import type { Team } from '$scripts/classes/team';
 	import { getContext } from 'svelte';
+	import type { Writable } from 'svelte/store';
 	import AtSscore from './ATSscore.svelte';
 	import AtStooltip from './ATStooltip.svelte';
 
-	let promise_status: ESPNStatus = getContext('promise_status');
-	let promise_scores: { home_score_data: ESPNScore; away_score_data: ESPNScore } =
-		getContext('promise_scores');
+	let promise_status: Writable<Promise<ESPNStatus>> = getContext('promise_status');
+	let promise_scores: Writable<
+		Promise<{ home_score_data: ESPNScore; away_score_data: ESPNScore }>
+	> = getContext('promise_scores');
 	let spread: number = getContext('spread');
 	let ATS_winner: string = getContext('ATS_winner');
 	let home_team: Team = getContext('home_team');
 	let away_team: Team = getContext('away_team');
 </script>
 
-{#await promise_status then status}
-	{#if status.type.description === 'Final'}
-		{#await promise_scores then scores}
+{#await $promise_status then status}
+	{#if status?.type?.description === 'Final'}
+		{#await $promise_scores then scores}
 			<AtSscore
 				home_or_away={'Away'}
 				team={away_team}
