f12b9c5e36d2941cf0564050ad91c0356886a012
James Moynihan <jrmoynihan@gmail.com>
2022-06-08 00:49:27 -0400

    feat(Components): :sparkles: Added a new component to select Teams
    Hopefully, this won't be needed much, but it didn't exist and seemed like it might need to at some point!

--- src/lib/components/containers/micro/TeamSelector.svelte
+++ src/lib/components/containers/micro/TeamSelector.svelte
@@ -1,59 +1,24 @@
 <script lang="ts">
-	import type { Team } from '$scripts/classes/team';
-	import { scrollToNextGame } from '$scripts/scrollAndFocus';
-	import { use_dark_theme } from '$scripts/store';
-	// import IntersectionObserver from 'svelte-intersection-observer';
-	import TeamImage from '../TeamImage.svelte';
-	import TeamNameImage from '../TeamNameImage.svelte';
-	import TeamRecord from './TeamRecord.svelte';
+	import { all_teams, selected_team } from '$scripts/store';
 
-	export let id: string;
-	export let selectedTeam: string;
-	export let homeOrAwayTeam: Team;
-	export let disabled: boolean;
-	export let showTeamNameImages: boolean;
-	// let element: HTMLElement;
-	// let showGameContainer = false;
+	export let is_admin_control = false;
 </script>
 
-<label
-	for="{id}-{homeOrAwayTeam}"
-	class="rounded dayShadow nightShadow"
-	class:pressed={selectedTeam === homeOrAwayTeam.abbreviation}
-	class:selected={selectedTeam === homeOrAwayTeam.abbreviation}
-	class:dark-mode={$use_dark_theme}
-	class:disabled
-	tabindex="0"
->
-	<input
-		id="{id}-{homeOrAwayTeam}"
-		type="radio"
-		bind:group={selectedTeam}
-		on:input={scrollToNextGame}
-		value={homeOrAwayTeam.abbreviation}
-		{disabled}
-	/>
-	<!-- <IntersectionObserver
-		once={true}
-		{element}
-		on:intersect={() => {
-			showGameContainer = true;
-		}}
-	>
-		{#if showGameContainer} -->
-	<TeamImage
-		team={homeOrAwayTeam}
-		grayscale={selectedTeam === homeOrAwayTeam.abbreviation && selectedTeam !== ''}
-	/>
-	{#if showTeamNameImages}
-		<TeamNameImage
-			team={homeOrAwayTeam}
-			rounded={true}
-			whiteBg={true}
-			grayscale={selectedTeam === homeOrAwayTeam.abbreviation && selectedTeam !== ''}
-		/>
-	{/if}
-	<!-- {/if}
-	</IntersectionObserver> -->
-	<TeamRecord showTeamAbbreviation={!showTeamNameImages} team={homeOrAwayTeam} />
-</label>
+<select bind:value={$selected_team} class:admin={is_admin_control} required>
+	<option value={''} disabled selected>Select a Team</option>
+	{#each $all_teams as team}
+		<option value={team}>{team.city} {team.name}</option>
+	{/each}
+</select>
+
+<style lang="scss">
+	select {
+		@include defaultSelect;
+		&:required:invalid {
+			content: 'Please select a team';
+		}
+	}
+	.admin {
+		@include admin;
+	}
+</style>
