8f5194c87ee54ccdcf6b4f23f0513b80ab1aea86
James Moynihan <jrmoynihan@gmail.com>
2022-06-09 01:45:09 -0400

    prettier formatting

--- src/routes/survivor/index.svelte
+++ src/routes/survivor/index.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-	import PageTitle from '$lib/components/misc/PageTitle.svelte';
+	import PageTitle from '$components/misc/PageTitle.svelte';
 </script>
 
 <PageTitle>Survivor</PageTitle>
+
 <svelte:head>
 	<title>Survivor Pool</title>
 </svelte:head>
