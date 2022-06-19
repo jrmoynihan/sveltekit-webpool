d4dd8c178e7343910d3dea48a61d5d354d04437b
James Moynihan <jrmoynihan@gmail.com>
2022-06-19 09:08:32 -0400

    ordered imports

--- src/routes/weekly/make-picks.svelte
+++ src/routes/weekly/make-picks.svelte
@@ -10,54 +10,47 @@
 </script>
 
 <script lang="ts">
+	import SubmitPicks from '$components/buttons/SubmitPicks.svelte';
+	import PickCounter from '$components/containers/micro/PickCounter.svelte';
+	import TiebreakerInput from '$components/inputs/TiebreakerInput.svelte';
+	import LoadingSpinner from '$components/misc/LoadingSpinner.svelte';
+	import ErrorModal from '$components/modals/ErrorModal.svelte';
 	import MatchupContainer from '$lib/components/containers/MatchupContainer.svelte';
 	import PageTitle from '$lib/components/misc/PageTitle.svelte';
 	import WeekSelect from '$lib/components/selects/WeekSelect.svelte';
-	import type { WeeklyPickDoc } from '$scripts/classes/picks';
-	import {
-		current_picks,
-		games_promise,
-		larger_than_mobile,
-		override_locked_picks,
-		picks_promise,
-		preferred_score_view,
-		tiebreaker_promise,
-		use_dark_theme,
-		selected_week,
-		selected_player,
-		selected_season_type,
-		tiebreaker_score_guess,
-		selected_year,
-		all_teams
-	} from '$scripts/store';
-	import { orderBy, where } from '@firebase/firestore';
-	import { tweened } from 'svelte/motion';
-	import { cubicOut } from 'svelte/easing';
-	import { isBeforeGameTime } from '$scripts/functions';
-	import {
-		airplaneDeparture,
-		all_icons,
-		bomb,
-		home,
-		policeCarLight
-	} from '$scripts/classes/constants';
-	import { onMount } from 'svelte';
-	import { defaultToast, getToast } from '$scripts/toasts';
-	import TiebreakerInput from '$components/inputs/TiebreakerInput.svelte';
-	import PickCounter from '$components/containers/micro/PickCounter.svelte';
-	import SubmitPicks from '$components/buttons/SubmitPicks.svelte';
-	import { fly } from 'svelte/transition';
-	import LoadingSpinner from '$components/misc/LoadingSpinner.svelte';
-	import type { Game } from '$scripts/classes/game';
-	import { getLocalScoreViewPreference, getLocalStorageItem } from '$scripts/localStorage';
-	import ErrorModal from '$components/modals/ErrorModal.svelte';
-	import { focusTiebreaker } from '$scripts/scrollAndFocus';
-	import { ErrorAndToast, myError, myLog } from '$scripts/logging';
-	import { getGameData, getPicksData, getTiebreakerData } from '$lib/scripts/weekly/weeklyPlayers';
-	import { createTiebreaker, createWeeklyPicksForPlayer } from '$lib/scripts/weekly/weeklyAdmin';
 	import type { Player } from '$lib/scripts/classes/player';
 	import type { WeeklyTiebreaker } from '$lib/scripts/classes/tiebreaker';
-
+	import { createTiebreaker,createWeeklyPicksForPlayer } from '$lib/scripts/weekly/weeklyAdmin';
+	import { getGameData,getPicksData,getTiebreakerData } from '$lib/scripts/weekly/weeklyPlayers';
+	import {
+	airplaneDeparture,
+	all_icons,
+	bomb,
+	home,
+	policeCarLight
+	} from '$scripts/classes/constants';
+	import type { Game } from '$scripts/classes/game';
+	import type { WeeklyPickDoc } from '$scripts/classes/picks';
+	import { isBeforeGameTime } from '$scripts/functions';
+	import { getLocalScoreViewPreference,getLocalStorageItem } from '$scripts/localStorage';
+	import { ErrorAndToast,myError,myLog } from '$scripts/logging';
+	import { focusTiebreaker } from '$scripts/scrollAndFocus';
+	import {
+	all_teams,current_picks,
+	games_promise,
+	larger_than_mobile,
+	override_locked_picks,
+	picks_promise,
+	preferred_score_view,selected_player,
+	selected_season_type,selected_week,selected_year,tiebreaker_promise,tiebreaker_score_guess,use_dark_theme
+	} from '$scripts/store';
+	import { defaultToast,getToast } from '$scripts/toasts';
+	import { orderBy,where } from '@firebase/firestore';
+	import { onMount } from 'svelte';
+	import { cubicOut } from 'svelte/easing';
+	import { tweened } from 'svelte/motion';
+	import { fly } from 'svelte/transition';
+	
 	export let query_year: string = null;
 	export let query_week: string = null;
 	let show_tiebreaker_input: boolean = false;
