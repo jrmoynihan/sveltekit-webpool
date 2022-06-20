adf372eb468e683d73ca29a81cc2da8316558880
James Moynihan <jrmoynihan@gmail.com>
2022-06-20 11:53:29 -0400

    added pick 6 rules container

---
+++ src/routes/pick6/rules.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import RulesContainer from '$containers/rules/RulesContainer.svelte';
+	import PageTitle from '$lib/components/misc/PageTitle.svelte';
+	import { pick6RulesCollection } from '$lib/scripts/firebase/collections';
+</script>
+
+<PageTitle>Pick6 Pool Rules</PageTitle>
+
+<RulesContainer rulesCollection={pick6RulesCollection} />
